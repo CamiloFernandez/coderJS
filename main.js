e70705684d90e4462b8be006c34e8bef4ffdfd5b
Camilo Fernandez <camiloferdez@hotmail.com>
2022-06-30 10:01:06 -0300

    Entrega Funciones

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-let num, range
+alert("En esta calculadora podrás ver cuanto dinero generas con los intereses de Mercado Pago o Ualá, solo ingresa los valores solicitados.")
+
+let capital, interes, tiempo;
+
+const calcularAnual = () => Math.round(capital*Math.pow((1+interes/100),tiempo))
+const calcularMensual = () => Math.round(capital*Math.pow((1+interes/100),tiempo/12)-capital)
+const calcularDiario = () => Math.round(capital*Math.pow((1+interes/100),tiempo/365)-capital)
 
 do{
-    num = parseFloat(prompt("Ingresa un numero del que quieras conocer su tabla"))
-    range = parseFloat(prompt("Elige hasta que multiplo quieres conocer"))
-}while(isNaN(num) || isNaN(range))
+    capital = parseInt(prompt("Ingrese el capital inicial"));
+    interes = parseInt(prompt("Ingrese el interés (sin el símbolo %)"));
+    tiempo = parseInt(prompt("Ingrese la cantidad de tiempo en años estimada"));
 
-for(i=1; i <= range; i++){
-    console.log(num +"X"+ i +"="+ (num*i))
-}
+    if(isNaN(capital) ||(capital<=0) || isNaN(interes) ||(interes<=0) || isNaN(tiempo) || (tiempo<=0)){
+        alert("Por favor ingrese números válidos.")
+    }
+}while(isNaN(capital) ||(capital<=0) || isNaN(interes) ||(interes<=0) || isNaN(tiempo) || (tiempo<=0))
+
+console.log("El total aproximado generado en el tiempo dado es: " +"$" + calcularAnual());
+console.log("El total aproximado generado mensualmente es: " +"$" + calcularMensual());
+console.log("El total aproximado generado diariamente es: " +"$" + calcularDiario());
