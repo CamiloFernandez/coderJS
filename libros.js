03de178eb658dabf089780c3e7abf51f2a28484b
Camilo Fernandez <camiloferdez@hotmail.com>
2022-07-06 19:57:37 -0300

    desafio Arrays

---
+++ libros.js
@@ -0,0 +1,9 @@
+class libros{
+  constructor(nombre, autor, codigo){
+    this.nombre = nombre,
+    this.autor = autor,
+    this.codigo = codigo
+  }
+
+}
+
--- main.js
+++ main.js
@@ -1,21 +1,26 @@
-alert("En esta calculadora podrás ver cuanto dinero generas con los intereses de Mercado Pago o Ualá, solo ingresa los valores solicitados.")
-
-let capital, interes, tiempo;
-
-const calcularAnual = () => Math.round(capital*Math.pow((1+interes/100),tiempo))
-const calcularMensual = () => Math.round(capital*Math.pow((1+interes/100),tiempo/12)-capital)
-const calcularDiario = () => Math.round(capital*Math.pow((1+interes/100),tiempo/365)-capital)
+let nombre, autor, codigo, registrar
+const resgistro = [] 
+function registrarLibro() {
+  do {
+  nombre = prompt("Ingrese el nombre del libro")
+  autor = prompt("Ingrese el nombre del autor")
+  codigo = prompt("Ingrese el codigo del libro")
+  if((nombre == "") || (autor == "") || isNaN(codigo)){
+    alert("Por favor ingresa los datos correctamente")
+  }
+} while ((nombre == "") || (autor == "") || isNaN(codigo))
+const libro = new libros(nombre, autor, codigo)
+resgistro.push(libro)
+console.log(resgistro)
+}
 
 do{
-    capital = parseInt(prompt("Ingrese el capital inicial"));
-    interes = parseInt(prompt("Ingrese el interés (sin el símbolo %)"));
-    tiempo = parseInt(prompt("Ingrese la cantidad de tiempo en años estimada"));
-
-    if(isNaN(capital) ||(capital<=0) || isNaN(interes) ||(interes<=0) || isNaN(tiempo) || (tiempo<=0)){
-        alert("Por favor ingrese números válidos.")
-    }
-}while(isNaN(capital) ||(capital<=0) || isNaN(interes) ||(interes<=0) || isNaN(tiempo) || (tiempo<=0))
-
-console.log("El total aproximado generado en el tiempo dado es: " +"$" + calcularAnual());
-console.log("El total aproximado generado mensualmente es: " +"$" + calcularMensual());
-console.log("El total aproximado generado diariamente es: " +"$" + calcularDiario());
+  registrar = prompt("Decea registrar otro libro? SI/NO").toLowerCase()
+  if(registrar == "si"){
+    registrarLibro()
+  }else if(registrar == "no"){
+    alert("¡Hasta luego!")
+  }else{
+    alert("respuesta invalida, responde SI o NO")
+  }
+}while((registrar == "")||(registrar == "si"))
