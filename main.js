02f2744b5f2b5af3c56536ed8e0c2f26264ec52f
Camilo Fernandez <camiloferdez@hotmail.com>
2022-07-06 22:17:27 -0300

    corregi horrores de ortografia

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function registrarLibro() {
   do {
   nombre = prompt("Ingrese el nombre del libro")
   autor = prompt("Ingrese el nombre del autor")
-  codigo = prompt("Ingrese el codigo del libro")
+  codigo = prompt("Ingrese el código del libro")
   if((nombre == "") || (autor == "") || isNaN(codigo)){
     alert("Por favor ingresa los datos correctamente")
   }
@@ -15,12 +15,12 @@ console.log(resgistro)
 }
 
 do{
-  registrar = prompt("Decea registrar otro libro? SI/NO").toLowerCase()
+  registrar = prompt("¿Desea registrar otro libro? SI/NO").toLowerCase()
   if(registrar == "si"){
     registrarLibro()
   }else if(registrar == "no"){
     alert("¡Hasta luego!")
   }else{
-    alert("respuesta invalida, responde SI o NO")
+    alert("Respuesta invalida, responde SI o NO")
   }
 }while((registrar == "")||(registrar == "si"))
